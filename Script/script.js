b0aa4cab7a26fa73acc2a9d90ccb0b81b3d8d123
Badboyyyyy <Pandeyprateek528@gmail.com>
2021-06-02 11:27:16 +0530

    done

---
+++ Script/script.js
@@ -0,0 +1,23 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+   autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:6,
+            nav:true,
+            loop:false
+        }
+    }
+})
